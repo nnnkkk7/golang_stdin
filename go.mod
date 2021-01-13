92a387aa6df643e86b9655684df2e026cc4c4156
nnnkkk7 <kurodanaoki0711pana@gmail.com>
2021-01-13 10:27:07 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nnnkkk7/golang_stdin
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	stdin := bufio.NewScanner(os.Stdin)
+	stdin.Scan()
+	text := stdin.Text()
+	fmt.Printf("%s", text)
+}
